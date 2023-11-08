c91526470df7a7591f711cf49eec2f6fe7e51e2d
soumyakanta rout <soumykantarout@gmail.com>
2023-11-08 15:06:03 +0530

    navbar edited

--- src/Detaileditem/itemlist.jsx
+++ src/Detaileditem/itemlist.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import myImage from './toy4.webp';
 import './itemlist.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import Navbar from '../components/Navbar';
 
 
 function ItemList() {
+
   // Sample items
   const items = [
     { id: 1, name: 'Item 1', price: 10, image: myImage },
@@ -21,6 +25,11 @@ function ItemList() {
   ];
 
   return (
+<>
+    <Navbar></Navbar>
+
+    
+    
     <div className="item-list-container">
       
       <div className="item-list">
@@ -30,11 +39,13 @@ function ItemList() {
             <div className="item-details">
               <h3>{item.name}</h3>
               <div className="price">Price: ${item.price}</div>
+              <FontAwesomeIcon icon={faCartShopping} />
             </div>
           </div>
         ))}
      </div>
     </div>
+    </>
   );
 }
 
