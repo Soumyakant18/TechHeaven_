a59362271dca3e1de9fd05d1937463724dc81380
soumyakanta rout <soumykantarout@gmail.com>
2023-11-18 23:51:57 +0530

    watch section , headphone section complete

--- src/components/Featuredp.jsx
+++ src/components/Featuredp.jsx
@@ -11,73 +11,43 @@ import productImage6 from '../assets/tws3.jpg'
 const productsData = [
     {
       id: 1,
-<<<<<<< HEAD
       name: 'AXL gaming 15v',
       price: '₹63,999',
-=======
-      name: 'AXL SLAYBOOK',
-      price: '$10.99',
->>>>>>> 4e06a1c7f7153f4e582205632ab2fe2f2e42049f
       image: productImage1,
       amount: 1,
     },
     {
       id: 2,
-<<<<<<< HEAD
       name: 'Leaf doublestorm',
       price: '₹1499',
-=======
-      name: 'LEAF 550H',
-      price: '$14.99',
->>>>>>> 4e06a1c7f7153f4e582205632ab2fe2f2e42049f
       image: productImage2,
       amount: 1,
     },
     {
       id: 3,
-<<<<<<< HEAD
       name: 'hp 15s fq5567',
       price: '₹39,899',
-=======
-      name: 'HP PAVILLION',
-      price: '$19.99',
->>>>>>> 4e06a1c7f7153f4e582205632ab2fe2f2e42049f
       image: productImage3,
       amount: 1,
     },
     {
       id: 4,
-<<<<<<< HEAD
       name: 'Apple 14 pro',
       price: '₹90,999',
-=======
-      name: 'IPHONE 14',
-      price: '$9.99',
->>>>>>> 4e06a1c7f7153f4e582205632ab2fe2f2e42049f
       image: productImage4,
       amount: 1,
     },
     {
       id: 5,
-<<<<<<< HEAD
       name: 'Boat hover',
       price: '₹2799',
-=======
-      name: 'BOAT RUNNER V3',
-      price: '$12.99',
->>>>>>> 4e06a1c7f7153f4e582205632ab2fe2f2e42049f
       image: productImage5,
       amount: 1,
     },
     {
       id: 6,
-<<<<<<< HEAD
       name: 'Alienware bashrock',
       price: '₹2299',
-=======
-      name: 'HAMMER H6',
-      price: '$22.99',
->>>>>>> 4e06a1c7f7153f4e582205632ab2fe2f2e42049f
       image: productImage6,
       amount: 1,
     },
